8bccfde41ae404c9295e671155662bc137be4f5c
ihell269 <ihell269@gmail.com>
2024-09-16 13:04:46 +0000

    push

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyC0-2JWRijFQmm3EMlxZ0yVrJkduqIgySM',
+      authDomain: 'sample-firebase-ai-app-5443a.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-5443a',
+      storageBucket: 'sample-firebase-ai-app-5443a.appspot.com',
+      messagingSenderId: '867561548959',
+      appId: '1:867561548959:web:68927de5ba46f659bb46aa',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyDzFnCnlEHeZkn4c0qL4KbcRnlbOPsq3_o',
+  debug_token: ''
+};
